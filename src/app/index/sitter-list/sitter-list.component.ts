e5aae7221c97272316b4678037ed43bbb08cb054
Alexandra <sasa.labusova@gmail.com>
2018-11-28 21:42:09 +0100

    sitters and pets headlines on index styling added

--- src/app/index/sitter-list/sitter-list.component.ts
+++ src/app/index/sitter-list/sitter-list.component.ts
@@ -1,6 +1,7 @@
 import { DataService } from './../../data.service';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'sitter-list',
   templateUrl: './sitter-list.component.html',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
   providers: [DataService]
 })
 export class SitterListComponent implements OnInit {
-
+  score : number = 3;
+  displayRatingScore = 4;
   sitters : any = [];
   
   constructor(private dataService:DataService) { }
