6436725349bd8de71443f5e12ea31469d882df5a
Alexandra <sasa.labusova@gmail.com>
2018-11-28 21:04:01 +0100

    yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  events: string[] = [];
-  opened: boolean;
+ 
 
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
